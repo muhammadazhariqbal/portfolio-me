ff4d8921c3c5d9038257384312864578d38490c2
Muhammad Azhar Iqbal <muhammad1azhar2@gmail.com>
2022-10-29 20:49:23 +0500

    php mail

--- assets/php/mail.php
+++ assets/php/mail.php
@@ -1,13 +1,9 @@
 <?php
+$to = "muhammad1azhar2@gmail.com";
+$subject = "My subject";
+$txt = "Hello world!";
+$headers = "From: webmaster@example.com" . "\r\n" .
+"CC: somebodyelse@example.com";
 
-$to 		= 'muhammad1azhar2@gmail.com';
-$headers	= 'FROM: "'.$email.'"';
-
-//All form values
-$name 		= $_POST['name'];
-$email 		= $_POST['email'];
-$subject 	= $_POST['subject'];
-$msg 		= $_POST['msg'];
-$output 	= "Name: ".$name."\nEmail: ".$email."\nSubject: ".$subject."\n\nMessage: ".$msg;
-
-$send		= mail($to, $name, $output, "From: $name <$email>");
+mail($to,$subject,$txt,$headers);
+?>
